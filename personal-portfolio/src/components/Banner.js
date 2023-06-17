3744660e140d77df82589a73f8a0680655a5030b
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-06-16 19:09:46 -0500

    change banner description to include sales past

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Greetings</span>
                 <h1>{`Hi! I'm Jonathan`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Front-End Developer", "Computer Science Student", "Salesman Turned Programmer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Welcome to my portfolio website! I am a passionate front-end developer with a keen eye for design and an unwavering commitment to crafting captivating user experiences. With a solid foundation in HTML, CSS, and JavaScript, I specialize in creating dynamic and interactive websites that seamlessly blend aesthetics with functionality. Moreover, I have honed my expertise in front-end libraries, notably React, utilizing its immense power to construct applications that are both scalable and easily maintainable.</p>
+                  <p>Welcome to my portfolio! I am a front-end developer with a unique background as a former salesman, bringing a fresh perspective to web development. Leveraging my strong communication and persuasion skills, I combine my passion for technology with an intuitive understanding of user needs to create visually appealing and user-friendly websites. With expertise in HTML, CSS, JavaScript, and front-end frameworks like React, I seamlessly blend my sales experience with my technical abilities to deliver captivating digital experiences that drive engagement and conversions.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
