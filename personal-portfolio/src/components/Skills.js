43d1800c9e0a58a0a39d99b8fa3810a042d1b1ca
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-06-23 14:51:09 -0500

    add and color icons

--- personal-portfolio/src/components/Skills.js
+++ personal-portfolio/src/components/Skills.js
@@ -1,6 +1,7 @@
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
+import meter4 from "../assets/img/meter4.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
@@ -49,7 +50,7 @@ export const Skills = () => {
                                 <h5>React</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter4} alt="Image" />
                                 <h5>Git</h5>
                             </div>
                         </Carousel>
