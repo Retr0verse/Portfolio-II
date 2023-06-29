be9756f2ad705de26d7bf9fdbc2a42e07dd1bf56
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-06-29 14:56:56 -0500

    add email for contact form

--- personal-portfolio/src/server.js
+++ personal-portfolio/src/server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: "mass3ffectai@gmail.com",
+    pass: "HARVARD7282baka$@-("
   },
 });
 
@@ -35,7 +35,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: "mass3ffectai@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
