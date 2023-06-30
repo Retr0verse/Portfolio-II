708b2454faea73518a7b4662a7cc04924eea6f59
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-06-30 17:23:06 -0500

    troubleshoot server contact form continue

--- personal-portfolio/server.js
+++ personal-portfolio/server.js
@@ -1,53 +1,20 @@
+const path = require('path');
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
+const PORT = process.env.PORT || 5000;
+
 // server used to send send emails
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
 
-const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: "mass3ffectai@gmail.com",
-    pass: "qztiklfuedhswodq"
-  },
+app.get('/api', (req, res) => {
+  res.json({message: 'Hello from server!'})
 });
 
-contactEmail.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready to Send");
-  }
+app.listen(PORT, () => {
+console.log('Server listening on port: ${PORT}')
 });
-//not working yet -- see if its due to 2 factor authentication
 
-router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
-  const mail = {
-    from: name,
-    to: "mass3ffectai@gmail.com",
-    subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
-           <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
-           <p>Message: ${message}</p>`,
-  };
-  contactEmail.sendMail(mail, (error) => {
-    if (error) {
-      res.json(error);
-    } else {
-      res.json({ code: 200, status: "Message Sent" });
-    }
-  });
-});
+
