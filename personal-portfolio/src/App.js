e9242eba17d2284a83bf08b30ede8d292390c327
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-07-03 00:55:15 -0500

    contact server running but not sending message, something went wrong error

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
@@ -8,7 +9,15 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
-function App() {
+
+const App = () => {
+ useEffect(() => {
+   fetch("/api")
+  .then((res) => res.json())
+  .then((data) => { console.log(data)})
+}, [])
+
+
   return (
     <div className="App">
       <NavBar />
@@ -20,5 +29,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
