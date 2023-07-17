2515ae465fd6a10265da112050f9f56d05ed6766
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-07-17 17:42:59 -0500

    removed description 2 for better responsiveness

--- personal-portfolio/src/components/ProjectCard.js
+++ personal-portfolio/src/components/ProjectCard.js
@@ -1,6 +1,6 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, description2, imgUrl, demoLink, repoLink }) => {
+export const ProjectCard = ({ title, description, imgUrl, demoLink, repoLink }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
@@ -8,8 +8,6 @@ export const ProjectCard = ({ title, description, description2, imgUrl, demoLink
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
-          <br />
-          <span>{description2}</span>
           <br />
           <a href={demoLink}>Live Demo</a>
           <br />
